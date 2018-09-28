f26d849d47b331dba16b51666a10926514e222e5
Akshata Mohanty <akshatamohanty@gmail.com>
2018-09-28 08:00:33 +0800

    Execute

--- src/app/shared/components/execute/execute.component.ts
+++ src/app/shared/components/execute/execute.component.ts
@@ -31,29 +31,89 @@ export class ExecuteComponent {
     @Input() flowchart: IFlowchart;
 
     execute($event): void {
-        // order the nodes based on edges array
 
-        // for each node,
-        // get code
-        // execute node
-        // update dependent nodes
-        this.flowchart.nodes.map((node: INode) => {
+        let all_nodes = this.flowchart.nodes;
+        let executed = [];
+        
+		while(executed.length < all_nodes.length){
+			for(let index=0; index < all_nodes.length; index++){
 
-            try{
+				let node = all_nodes[index];
+				if(executed.indexOf(index) > -1){
+					//do nothing
+				}
+				else{
 
-                //new Function ([arg1[, arg2[, ...argN]],] functionBody)
-                const fn = new Function('__MODULES__', CodeUtils.getNodeCode(node));
-                let results = fn(Modules);
-                node.outputs.map( (oup) => {
-                    oup.value = results[oup.name];
-                });
-                
-            }
-            catch(ex){
-                console.warn(`${node.name} errored`);
-            }
-            
-        });
+					// check if all inputs have valid inputs
+					// if yes, execute add to executed
+					// if no, set flag to true 
+					// check status of the node; don't rerun
+					if( !node.enabled /* or hasFnOutput */ ){
+						// do nothing
+						executed.push(index);
+					}
+					else{
+
+						let flag = true;
+						let inputs = node.getInputs();
+						for(let i=0; i < inputs.length; i++){
+							let inp = inputs[i];
+
+							if(inp.getValue() && inp.getValue()["port"] && !inp.isFunction()){
+								flag = false;
+								break;
+							}
+						}
+
+						if(flag){
+                            console.log(`${node.getName()} executing...`);
+                            this.executeNode(node);
+							this.updateDependentInputs(node, index); 
+							executed.push(index);
+						}
+
+					}
+				}
+			} 
+		}
 
     }
+
+    executeNode(node: INode): void{
+        try{
+
+            //new Function ([arg1[, arg2[, ...argN]],] functionBody)
+            const fn = new Function('__MODULES__', CodeUtils.getNodeCode(node));
+            let results = fn(Modules);
+            node.outputs.map( (oup) => {
+                oup.value = results[oup.name];
+            });
+            
+        }
+        catch(ex){
+            console.warn(`${node.name} errored`);
+        }
+    }
+
+    updateDependentInputs(node: INode, originalRank: number): void{
+
+		let selectedEdges: IEdge[] = this.flowchart.edges.filter(function(edge){
+			return edge.output_address[0] == originalRank;
+		});
+
+		for( let e=0;  e < selectedEdges.length; e++ ){
+
+			let edge: IEdge = selectedEdges[e];
+			let inputNode: INode = this.flowchart.nodes[ edge.input_address[0] ];
+
+			// set computed value of port
+			// should this be from within the node?
+			let outputPort =  node.outputs[ edge.output_address[1] ];
+			let inputPort = inputNode.inputs[ edge.input_address[1] ];
+
+
+			inputPort.value =  JSON.parse(JSON.stringify( outputPort.value )) ;
+
+		}
+	}
 }

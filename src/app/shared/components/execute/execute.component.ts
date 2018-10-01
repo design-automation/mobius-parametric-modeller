9737e2ba30366bb694e79df11973520ae25bdf06
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2018-10-01 11:23:29 +0800

    execute

--- src/app/shared/components/execute/execute.component.ts
+++ src/app/shared/components/execute/execute.component.ts
@@ -33,76 +33,8 @@ export class ExecuteComponent {
     @Input() flowchart: IFlowchart;
 
     execute($event): void {      
-        // order the nodes based on edges array
-
-        // for each node,
-        // get code
-        // execute node
-        // update dependent nodes
         this.flowchart.nodes.map((node: INode) => {
-            let prodArr: string[] = [''];
-            try{
-                //new Function ([arg1[, arg2[, ...argN]],] functionBody)
-                const fn = new Function('__MODULES__', '__PRODARR__', CodeUtils.getNodeCode(node));
-                let results = fn(Modules, prodArr);
-                node.outputs.map( (oup) => {
-                    if (typeof results[oup.name] === 'number' || results[oup.name] === undefined){
-                        oup.value = results[oup.name];
-                    } else if (typeof results[oup.name] === 'string'){
-                        oup.value = '"' + results[oup.name] + '"';
-                    } else if (results[oup.name].constructor === [].constructor){
-                        oup.value = '[' + results[oup.name] + ']';
-                    } else if (results[oup.name].constructor === {}.constructor) {
-                        oup.value = JSON.stringify(results[oup.name]);
-                    } else {
-                        oup.value = results[oup.name];
-                    }
-                });
-                
-            }
-            catch(ex){
-                node.hasError = true;
-                //console.warn(`${node.name} errored`);
-
-                // Unexpected Identifier
-                // Unexpected token
-                let prodWithError: string = prodArr[0]; 
-                let markError = function(prod: IProcedure, id: string){
-                    if(prod["ID"] && id && prod["ID"] == id){
-                        prod.hasError = true;
-                    }
-                    if(prod.hasOwnProperty('children')){
-                        prod.children.map(function(p){
-                            markError(p, id);
-                        });
-                    }
-                }
-                if(prodWithError != ''){
-                    node.procedure.map(function(prod: IProcedure){
-                        if(prod["ID"] == prodWithError){
-                            prod.hasError = true;
-                        }
-                        if(prod.hasOwnProperty('children')){
-                            prod.children.map(function(p){
-                                markError(p, prodWithError);
-                            })
-                        }
-                    });
-                }
-                let error: Error;
-                if(ex.toString().indexOf("Unexpected identifier") > -1){
-                    error = new Error("Unexpected Identifier error. Did you declare everything? Check that your strings are enclosed in quotes (\")");
-                }
-                else if(ex.toString().indexOf("Unexpected token") > -1){
-                    error = new Error("Unexpected token error. Check for stray spaces or reserved keywords?");
-                }
-                else{
-                     error = new Error(ex);
-                }
-                throw error;
-                
-            }
-            
+            this.executeNode(node);
         });
     /*
     execute($event): void {
@@ -151,87 +83,72 @@ export class ExecuteComponent {
 				}
 			} 
 		}
-
-    }
-
-    executeNode(node: INode): void{
-        try{
-
-            //new Function ([arg1[, arg2[, ...argN]],] functionBody)
-            const fn = new Function('__MODULES__', CodeUtils.getNodeCode(node));
-            let results = fn(Modules);
-            node.outputs.map( (oup) => {
-                oup.value = results[oup.name];
-            });
-            
-        }
-        catch(ex){
-            console.warn(`${node.name} errored`);
-        }
-    }
-}
-
-        let all_nodes = this.flowchart.nodes;
-        let executed = [];
-        
-		while(executed.length < all_nodes.length){
-			for(let index=0; index < all_nodes.length; index++){
-
-				let node = all_nodes[index];
-				if(executed.indexOf(index) > -1){
-					//do nothing
-				}
-				else{
-
-					// check if all inputs have valid inputs
-					// if yes, execute add to executed
-					// if no, set flag to true 
-					// check status of the node; don't rerun
-					if( !node.enabled /* or hasFnOutput */ /*){
-						// do nothing
-						executed.push(index);
-					}
-					else{
-
-						let flag = true;
-						let inputs = node.getInputs();
-						for(let i=0; i < inputs.length; i++){
-							let inp = inputs[i];
-
-							if(inp.getValue() && inp.getValue()["port"] && !inp.isFunction()){
-								flag = false;
-								break;
-							}
-						}
-
-						if(flag){
-                            console.log(`${node.getName()} executing...`);
-                            this.executeNode(node);
-							this.updateDependentInputs(node, index); 
-							executed.push(index);
-						}
-
-					}
-				}
-			} 
-		}
     */
 
     }
 
     executeNode(node: INode): void{
+        let prodArr: string[] = [''];
         try{
-
             //new Function ([arg1[, arg2[, ...argN]],] functionBody)
-            const fn = new Function('__MODULES__', CodeUtils.getNodeCode(node));
-            let results = fn(Modules);
+            const fn = new Function('__MODULES__', '__PRODARR__', CodeUtils.getNodeCode(node));
+            let results = fn(Modules, prodArr);
             node.outputs.map( (oup) => {
-                oup.value = results[oup.name];
+                if (typeof results[oup.name] === 'number' || results[oup.name] === undefined){
+                    oup.value = results[oup.name];
+                } else if (typeof results[oup.name] === 'string'){
+                    oup.value = '"' + results[oup.name] + '"';
+                } else if (results[oup.name].constructor === [].constructor){
+                    oup.value = '[' + results[oup.name] + ']';
+                } else if (results[oup.name].constructor === {}.constructor) {
+                    oup.value = JSON.stringify(results[oup.name]);
+                } else {
+                    oup.value = results[oup.name];
+                }
             });
             
         }
         catch(ex){
-            console.warn(`${node.name} errored`);
+            node.hasError = true;
+            //console.warn(`${node.name} errored`);
+
+            // Unexpected Identifier
+            // Unexpected token
+            let prodWithError: string = prodArr[0]; 
+            let markError = function(prod: IProcedure, id: string){
+                if(prod["ID"] && id && prod["ID"] == id){
+                    prod.hasError = true;
+                }
+                if(prod.hasOwnProperty('children')){
+                    prod.children.map(function(p){
+                        markError(p, id);
+                    });
+                }
+            }
+            if(prodWithError != ''){
+                node.procedure.map(function(prod: IProcedure){
+                    if(prod["ID"] == prodWithError){
+                        prod.hasError = true;
+                    }
+                    if(prod.hasOwnProperty('children')){
+                        prod.children.map(function(p){
+                            markError(p, prodWithError);
+                        })
+                    }
+                });
+            }
+            let error: Error;
+            if(ex.toString().indexOf("Unexpected identifier") > -1){
+                error = new Error("Unexpected Identifier error. Did you declare everything? Check that your strings are enclosed in quotes (\")");
+            }
+            else if(ex.toString().indexOf("Unexpected token") > -1){
+                error = new Error("Unexpected token error. Check for stray spaces or reserved keywords?");
+            }
+            else{
+                 error = new Error(ex);
+            }
+            throw error;
+            
         }
     }
 

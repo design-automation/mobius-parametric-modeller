f051894f61354a2bb5e372b2b3f4b5059133f544
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2018-10-24 11:59:25 +0800

    prep for UI changes 2

--- src/app/mViewer/mobius-viewer.module.ts
+++ src/app/mViewer/mobius-viewer.module.ts
@@ -9,7 +9,7 @@ import { GSViewer } from './viewers/gs-viewer/gs-viewer.module';
 
 import {MobiusCesium} from './viewers/mobius-cesium/mobius-cesium.module';
 
-import { TextViewerComponent, JSONViewerComponent } from './viewers';
+import { TextViewerComponent} from './viewers';
 //import { VisualiseComponent } from "./viewers/mobius-cesium/setting/visualise.component";
 //import { AttributesComponent } from "./viewers/mobius-cesium/setting/attributes.copmponent";
 
@@ -17,7 +17,6 @@ import { TextViewerComponent, JSONViewerComponent } from './viewers';
   declarations: [
     ViewerContainerComponent,
     TextViewerComponent,
-    JSONViewerComponent,
   ],
   exports: [ ViewerContainerComponent ],
   imports: [ CommonModule, 

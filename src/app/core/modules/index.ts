d8f8057cb7d3c328889e46dc39c018c4c6beb187
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2018-11-27 09:53:32 +0800

    input - output node fix

--- src/app/core/modules/index.ts
+++ src/app/core/modules/index.ts
@@ -1,7 +1,10 @@
 import * as functions from "./functions";
 export {functions};
+import * as Input from './Input';
+export {Input}
+import * as Output from './Output';
+export {Output}
 
-export * from './input'; //./old/point
-export * from './output';
+//./old/point
 export * from './_parameterTypes';
 //./old/ray./old/query./old/string./old/topo

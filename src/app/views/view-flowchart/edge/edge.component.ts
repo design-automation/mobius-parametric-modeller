ab4db10fe06f620cf226834be996fc085cdd0185
Derek <derekpung@hotmail.com>
2018-12-18 16:23:32 +0800

    toolset css

--- src/app/views/view-flowchart/edge/edge.component.ts
+++ src/app/views/view-flowchart/edge/edge.component.ts
@@ -4,31 +4,7 @@ import { IEdge } from '@models/edge';
 @Component({
   selector: '[edge]',
   templateUrl: './edge.component.html',
-  styles: [`
-    .edge{
-        fill: none;
-        stroke: rgb(80, 80, 80);
-        stroke-linecap: round;
-        stroke-linejoin: round;
-        stroke-width: 2px;
-        opacity: 1;
-        pointer-events: stroke;
-        marker-end: url(#arrow);
-    }
-    .inviEdge{
-        fill: none;
-        stroke: gray;
-        stroke-width: 30px;
-        opacity: 0;
-        pointer-events: stroke;
-    }
-    .selected{
-        stroke: rgb(0, 0, 150);
-        opacity: 1;
-        marker-end: url(#arrow_selected);
-
-    }
-  `]
+  styleUrls: ['./edge.component.scss']
 })
 export class EdgeComponent {
 

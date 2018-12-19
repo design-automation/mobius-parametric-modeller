ec69da837c7e32eb703125f7496bf89b39857f4f
Patrick Janssen <akiphtj@nus.edu.sg>
2018-12-19 12:22:11 +0800

    fixed getPositions() bug, started  obj export

--- src/libs/filesys/download.ts
+++ src/libs/filesys/download.ts
@@ -7,7 +7,7 @@ export function download(data: string, filename: string): boolean {
       const data_type = 'text/plain;charset=utf-8';
       const data_bom = decodeURIComponent('%ef%bb%bf');
       if (window.navigator.msSaveBlob) {
-          const blob = new Blob([data_bom + data],{type: data_type });
+          const blob = new Blob([data_bom + data], { type: data_type } );
           window.navigator.msSaveBlob(blob, data);
       } else {
           const link = document.createElement('a');

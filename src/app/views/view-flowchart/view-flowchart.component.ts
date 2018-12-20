e909184e585245b6fa4c0a2f84e43f753aecfbaa
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2018-12-20 10:50:28 +0800

    flowchart centering fix

--- src/app/views/view-flowchart/view-flowchart.component.ts
+++ src/app/views/view-flowchart/view-flowchart.component.ts
@@ -449,7 +449,7 @@ export class ViewFlowchartComponent implements OnInit, AfterViewInit {
             }
         }
         frame[2] += 100;
-        frame[3] += 150;
+        frame[3] += 100;
 
         // calculate the zoom to fit the whole flowchart
         const bRect = <DOMRect>this.canvas.getBoundingClientRect();
@@ -459,20 +459,32 @@ export class ViewFlowchartComponent implements OnInit, AfterViewInit {
         if (zoom > heightZoom) { zoom = heightZoom; }
         if (zoom > this.maxZoom) { zoom = this.maxZoom; } else if (zoom < this.minZoom) { zoom = this.minZoom; }
 
+        const boundingDiv = <DOMRect>document.getElementById('flowchart-main-container').getBoundingClientRect();
+
+        frame[0] = (boundingDiv.width  - (frame[2] - frame[0]) * ctm.a * zoom / this.zoom) / 2 - frame[0] * ctm.a * zoom / this.zoom;
+        frame[1] = (boundingDiv.height - (frame[3] - frame[1]) * ctm.d * zoom / this.zoom) / 2 - frame[1] * ctm.a * zoom / this.zoom;
+
+        // -( frame[0] * ctm.a * zoom / this.zoom )
+
+        /*
         // calculate the difference between height and width, if height is bigger than width,
         // centering the flowchart based on the difference
         const height_width_diff = ((frame[3] - frame[1]) - (frame[2] - frame[0])) / 2;
         if (height_width_diff > 0) {
             frame[0] -= height_width_diff;
         }
+        */
 
         // if the minX or minY goes below 0 (outside of svg frame), change them back to 0
-        if (frame[0] < 0) { frame[0] = 0; }
-        if (frame[1] < 0) { frame[1] = 0; }
+        if (frame[0] > 0) { frame[0] = 0; }
+        if (frame[1] > 0) { frame[1] = 0; }
 
         // transform
-        this.dataService.flowchartPos = `matrix(${zoom},0,0,${zoom},${-frame[0] * ctm.a *
-            zoom / this.zoom},${-frame[1] * ctm.a * zoom / this.zoom})`;
+        /*
+        this.dataService.flowchartPos = `matrix(${zoom},0,0,${zoom},${
+            -frame[0] * ctm.a * zoom / this.zoom},${-frame[1] * ctm.a * zoom / this.zoom})`;
+            */
+        this.dataService.flowchartPos = `matrix(${zoom},0,0,${zoom},${frame[0]},${frame[1]})`;
         this.canvas.style.transform = this.dataService.flowchartPos;
         this.zoom = zoom;
     }

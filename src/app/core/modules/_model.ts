a85a3c81f66a8237e6394f7be99711c166479c40
Derek <derekpung@hotmail.com>
2019-01-04 17:08:44 +0800

    error handling inline attrib

--- src/app/core/modules/_model.ts
+++ src/app/core/modules/_model.ts
@@ -1,6 +1,7 @@
 import { GIModel } from '@libs/geo-info/GIModel';
 import { EAttribDataTypeStrs, TAttribDataTypes, EAttribNames, EEntityTypeStr, TId } from '@libs/geo-info/common';
 import { idBreak } from '@libs/geo-info/id';
+import { checkIDs, checkCommTypes } from './_check_args';
 
 //  ===============================================================================================================
 //  Functions used by Mobius
@@ -61,6 +62,11 @@ export function __stringify__(__model__: GIModel): string {
  */
 export function __setAttrib__(__model__: GIModel, entities: TId|TId[],
     attrib_name: string, attrib_value: TAttribDataTypes, attrib_index?: number): void {
+    // --- Error Check ---
+    const fn_name = 'Inline.__setAttrib__';
+    checkIDs(fn_name, 'entities', entities, ['isID', 'isIDList'], 'all');
+    checkCommTypes(fn_name, 'attrib_value', attrib_value, ['isString', 'isNumber', 'isStringList', 'isNumberList']);
+    // --- Error Check ---
     // console.log("__model__", __model__);
     // console.log("entities", entities);
     // console.log("attrib_name", attrib_name);
@@ -68,6 +74,9 @@ export function __setAttrib__(__model__: GIModel, entities: TId|TId[],
     if (!Array.isArray(entities)) {
         const [ent_type_str, index]: [EEntityTypeStr, number] = idBreak(entities as TId);
         if (attrib_index !== null && attrib_index !== undefined) {
+            // --- Error Check ---
+            checkCommTypes(fn_name, 'attrib_index', attrib_index, ['isNumber']);
+            // --- Error Check ---
             const value_arr = __model__.attribs.query.getAttribValue(ent_type_str, attrib_name, index);
             value_arr[attrib_index] = attrib_value;
             __model__.attribs.add.setAttribValue(ent_type_str, index, attrib_name, value_arr);
@@ -86,12 +95,20 @@ export function __setAttrib__(__model__: GIModel, entities: TId|TId[],
  */
 export function __getAttrib__(__model__: GIModel, entities: TId|TId[],
     attrib_name: string, attrib_index?: number): TAttribDataTypes|TAttribDataTypes[] {
+    // --- Error Check ---
+    const fn_name = 'Inline.__getAttrib__';
+    checkIDs(fn_name, 'entities', entities, ['isID', 'isIDList'], 'all');
+    checkCommTypes(fn_name, 'attrib_name', attrib_name, ['isString']);
+    // --- Error Check ---
     // console.log("__model__", __model__);
     // console.log("entities", entities);
     // console.log("attrib_name", attrib_name);
     if (!Array.isArray(entities)) {
         const [ent_type_str, index]: [EEntityTypeStr, number] = idBreak(entities as TId);
         if (attrib_index !== null && attrib_index !== undefined) {
+            // --- Error Check ---
+            checkCommTypes(fn_name, 'attrib_index', attrib_index, ['isNumber']);
+            // --- Error Check ---
             return __model__.attribs.query.getAttribValue(ent_type_str, attrib_name, index)[attrib_index];
         } else {
             return __model__.attribs.query.getAttribValue(ent_type_str, attrib_name, index);

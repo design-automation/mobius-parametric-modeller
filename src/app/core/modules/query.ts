5b272998412e1d72de020df508ee2be7a93beac0
Patrick Janssen <akiphtj@nus.edu.sg>
2019-01-08 12:03:22 +0800

    added toolset expressions

--- src/app/core/modules/query.ts
+++ src/app/core/modules/query.ts
@@ -46,16 +46,23 @@ function _convertSelectToEEntityTypeStr(select: _EQuerySelect): EEntityTypeStr {
     }
 }
 /**
- * Queries the id of any entity based on attribute name.
+ * Returns a list of entities based on a query expression.
+ * The query expression should follow the following format: #@name == value,
+ * where 'name' is the attribute name, and 'value' is the attribute value.
+ * If the attribute value is a string, then in must be in qoutes, as follows: #@name == 'str_value'.
+ * The '==' is the comparison operator. The other comparison operators are: !=, >, >=, <, =<.
+ * Entities can be search using multiple query expressions, as follows:  #@name1 == value1 &&  #@name2 == value2.
+ * Query expressions can be combine with either && (and) and || (or), where
+ * && takes precedence over ||.
  * @param __model__
  * @param select Enum, specifies what type of entities will be returned.
  * @param entities List of entities to be searched. If 'null' (without quotes), list of all entities in the model.
- * @param attrib_query Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
+ * @param query_expr Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
  * @returns List of id.
  * @example positions = query.Get(positions, polyline1, #@xyz[2]>10)
  * @example_info Returns a list of positions defined by polyline1 where the z-coordinate is more than 10.
  */
-export function Get(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], attrib_query: TQuery): TId[] {
+export function Get(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], query_expr: TQuery): TId[] {
     // get the select ent_type_str
     const select_ent_type_str: EEntityTypeStr = _convertSelectToEEntityTypeStr(select);
     // get the list of entities
@@ -74,45 +81,56 @@ export function Get(__model__: GIModel, select: _EQuerySelect, entities: TId|TId
         }
     }
     // check if the query is null
-    if (attrib_query === null || attrib_query === undefined) {
+    if (query_expr === null || query_expr === undefined) {
         return found_entities_i.map( entity_i => select_ent_type_str + entity_i);
     }
     // do the query on the list of entities
-    const query_result: number[] = __model__.attribs.query.queryAttribs(select_ent_type_str, attrib_query, found_entities_i);
+    const query_result: number[] = __model__.attribs.query.queryAttribs(select_ent_type_str, query_expr, found_entities_i);
     return query_result.map( entity_i => select_ent_type_str + entity_i);
 }
 /**
- * Queries the number of any entity based on attribute name.
+ * Returns the number of entities based on a query expression.
+ * The query expression should follow the following format: #@name == value,
+ * where 'name' is the attribute name, and 'value' is the attribute value.
+ * If the attribute value is a string, then in must be in qoutes, as follows: #@name == 'str_value'.
+ * The '==' is the comparison operator. The other comparison operators are: !=, >, >=, <, =<.
+ * Entities can be search using multiple query expressions, as follows:  #@name1 == value1 &&  #@name2 == value2.
+ * Query expressions can be combine with either && (and) and || (or), where
+ * && takes precedence over ||.
+ * 
  * @param __model__
  * @param select Enum, specifies what type of entities are to be counted.
  * @param entities List of entities to be searched. If 'null' (without quotes), list of all entities in the model.
- * @param attrib_query Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
+ * @param query_expr Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
  * @returns Number of entities.
  * @example num_ents = query.Count(positions, polyline1, #@xyz[2]>10)
  * @example_info Returns the number of positions defined by polyline1 where the z-coordinate is more than 10.
  */
-export function Count(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], attrib_query: TQuery): number {
+export function Count(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], query_expr: TQuery): number {
     // --- Error Check ---
     checkIDs('query.Get', 'entities', entities, ['isID', 'isIDList'], 'all');
     // --- Error Check ---
-    return Get(__model__, select, entities, attrib_query).length;
+    return Get(__model__, select, entities, query_expr).length;
 }
 export enum _ESortMethod {
     'DESCENDING' = 'descending',
     'ASCENDING' = 'ascending'
 }
 /**
- * Sorts entities
+ * Sorts entities based on a sort expression.
+ * The sort expression should follow the following format: #@name, where 'name' is the attribute name.
+ * Entities can be sorted using multiple sort expresssions as follows: #@name1 && #@name2.
+ * If the attributes is a list, and index can also be specified as follows: #@name1[index].
  * @param __model__
  * @param select Enum, specifies what type of entities are to be counted.
  * @param entities List of entities to be searched. If 'null' (without quotes), list of all entities in the model.
- * @param attrib_query Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
+ * @param sort_expr Attribute condition. If 'null' (without quotes), no condition is set; list of all search entities is returned.
  * @param method Enum
  * @returns Sorted entities.
- * @example sorted_list = query.Sort(positions, polyline1, #@xyz[2])
- * @example_info Returns the number of positions defined by polyline1 where the z-coordinate is more than 10.
+ * @example sorted_list = query.Sort(positions, polyline1, #@xyz[2], descending)
+ * @example_info Returns a list of positions in polyline1, sorted according to the descending z value.
  */
-export function Sort(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], attrib_query: TQuery, method: _ESortMethod): TId[] {
+export function Sort(__model__: GIModel, select: _EQuerySelect, entities: TId|TId[], sort_expr: TQuery, method: _ESortMethod): TId[] {
     // get the select ent_type_str
     const select_ent_type_str: EEntityTypeStr = _convertSelectToEEntityTypeStr(select);
     // get the list of entities
@@ -131,7 +149,7 @@ export function Sort(__model__: GIModel, select: _EQuerySelect, entities: TId|TI
         }
     }
     // check if the query is null
-    if (attrib_query === null || attrib_query === undefined) {
+    if (sort_expr === null || sort_expr === undefined) {
         found_entities_i.sort();
         if (method === _ESortMethod.ASCENDING) {
             found_entities_i.reverse();
@@ -141,6 +159,6 @@ export function Sort(__model__: GIModel, select: _EQuerySelect, entities: TId|TI
     // do the sort on the list of entities
     const _sort_method: ESort = (method === _ESortMethod.DESCENDING) ? ESort.DESCENDING : ESort.ASCENDING;
     const sort_result: number[] = __model__.attribs.query.sortByAttribs(
-        select_ent_type_str, attrib_query, found_entities_i, _sort_method);
+        select_ent_type_str, sort_expr, found_entities_i, _sort_method);
     return sort_result.map( entity_i => select_ent_type_str + entity_i);
 }

c615aca857fea48a0eb747223d644a01e67e1438
Patrick Janssen <akiphtj@nus.edu.sg>
2019-01-11 10:02:46 +0800

    added inline functions, randPick(), shuffle(), zip(), zip2()

--- src/app/core/modules/_list.ts
+++ src/app/core/modules/_list.ts
@@ -2,16 +2,7 @@
  * list functions that obtain and return information from an input list. Does not modify input list.
  */
 
-/**
- * Creates a new list of integer numbers between two bounds.
- * Lower bound is inclusive and upper bound is exclusive.
- *
- * @param min Lower bound integer.
- * @param max Upper bound integer.
- * @returns New list.
- * @example list = list.range(0,5)
- * @example_info Expected value of list is [0,1,2,3,4].
- */
+
 export function range(min: number, max: number): number[] {
     if (min === undefined) { throw new Error('Invalid arg: min must be defined.'); }
     if (max === undefined) { throw new Error('Invalid arg: max must be defined.'); }
@@ -19,17 +10,28 @@ export function range(min: number, max: number): number[] {
     if (len <= 0) {return []; }
     return Array.apply(0, new Array(len)).map((v, i) => i + min);
 }
-/**
- * Creates a new list of integer numbers between two bounds.
- * Lower bound is inclusive and upper bound is exclusive.
- *
- * @param min Lower bound integer.
- * @param max Upper bound integer.
- * @returns New list.
- * @example list = list.range(0,5)
- * @example_info Expected value of list is [0,1,2,3,4].
- */
+
 export function length(list: any[]): number {
     if (list === undefined) { throw new Error('Invalid arg: list must be defined.'); }
     return list.length;
+}
+
+export function shuffle(list: any[]): any[] {
+    const new_list: any[] = list.slice();
+    new_list.sort(() => .5 - Math.random());
+    return new_list;
+}
+
+export function zip(lists: any[][]): any[] {
+    const shortest = lists.length === 0 ? [] : lists.reduce((a, b) => {
+        return a.length < b.length ? a : b;
+    });
+    return shortest.map((_, i) => lists.map(array => array[i] ));
+}
+
+export function zip2(lists: any[][]): any[] {
+    const longest = lists.length === 0 ? [] : lists.reduce((a, b) => {
+        return a.length > b.length ? a : b;
+    });
+    return longest.map((_, i) => lists.map(array => array[i] ));
 }
--- src/app/core/modules/_rand.ts
+++ src/app/core/modules/_rand.ts
@@ -1,15 +1,11 @@
 import * as mathjs from 'mathjs';
 
-//._mathjs.random;
-//._mathjs.randomInt;
-//._mathjs.pickRandom;
-
 export function rand(min: number, max: number): number {
     return mathjs.random(min, max);
 }
 export function randInt(min: number, max: number): number {
     return mathjs.randomInt(min, max);
 }
-export function randPick(list: any[]): number {
-    return mathjs.pickRandom(list);
+export function randPick(list: any[], num: number): number {
+    return mathjs.pickRandom(list, num);
 }

faece4739dd71b34f2c3789a20814b26660aae1b
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-01-24 16:41:02 +0800

    unit test update

--- src/app/shared/components/execute/execute.component.spec.ts
+++ src/app/shared/components/execute/execute.component.spec.ts
@@ -1,5 +1,4 @@
 import { DataService } from '../../../shared/services/data.service';
-import * as galleryUrl from '@assets/gallery/__config__.json';
 import { Router } from '@angular/router';
 import { TestBed, ComponentFixture, fakeAsync, tick, async } from '@angular/core/testing';
 import { LoadUrlComponent } from '../file/loadurl.component';
@@ -8,6 +7,9 @@ import { MatIconModule } from '@angular/material';
 import { GoogleAnalyticsService } from '@shared/services/google.analytics';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { FlowchartUtils } from '@models/flowchart';
+
+import * as galleryUrl from '@assets/gallery/__config__.json';
+import * as testUrl from '@assets/unit_tests/unit_test.json';
 
 describe('Execute Component test', () => {
     let loadURLfixture:   ComponentFixture<LoadUrlComponent>;
@@ -39,7 +41,8 @@ describe('Execute Component test', () => {
         spinnerFixture = TestBed.createComponent(SpinnerComponent);
         router = TestBed.get(Router);
         dataService = TestBed.get(DataService);
-        dataService.file.flowchart = undefined;
+        window['ga'] = function() { };
+        // dataService.file.flowchart = undefined;
     });
 
     for (const exampleSet of galleryUrl.data) {
@@ -49,9 +52,9 @@ describe('Execute Component test', () => {
             if (f.indexOf('node=') !== -1) {
                 nodeCheck = true;
             }
-            it('load and execute file: ' + f.split('.mob')[0], async (done: DoneFn) => {
+            it('load and execute gallery file: ' + f.split('.mob')[0], async (done: DoneFn) => {
                 await loadURLfixture.componentInstance.loadStartUpURL(`?file=${exampleSet.link}${f}`);
-                const spy = router.navigate as jasmine.Spy;
+                const rSpy = router.navigate as jasmine.Spy;
                 expect(dataService.file.flowchart).toBeDefined(`Unable to load ${f.split('.mob')[0]}.mob`);
                 if (dataService.file.flowchart) {
                     let nodeProcedures = 0;
@@ -61,14 +64,84 @@ describe('Execute Component test', () => {
                     expect(nodeProcedures > dataService.flowchart.nodes.length + 1).toBe(true,
                             `${f.split('.mob')[0]}.mob is an empty flowchart`);
                     if (nodeCheck) {
-                        expect(spy.calls.first().args[0][0]).toBe('/editor', 'Navigate to editor');
+                        expect(rSpy.calls.first().args[0][0]).toBe('/editor', 'Needs to navigate to editor');
                     }
-                    // await executeFixture.componentInstance.execute();
+                    await executeFixture.componentInstance.execute(true);
+                    expect(f.split('.mob')[0]).toBe(dataService.file.name,
+                        'Loaded file name and the file name in dataService do not match.');
+                    expect(dataService.flowchart.nodes[dataService.flowchart.nodes.length - 1].model).toBeDefined(
+                        `Execute fails. The end node model is not defined.`);
                 }
                 done();
             });
         }
     }
 
+    for (const test of testUrl.test_data) {
+        let testName: any = test.url.split('/');
+        testName = testName[testName.length - 1];
+        let nodeCheck = false;
+        if (test.url.indexOf('node=') !== -1) {
+            nodeCheck = true;
+        }
+        it('load and execute test file: ' + testName.split('.mob')[0], async (done: DoneFn) => {
+            await loadURLfixture.componentInstance.loadStartUpURL(`?file=${test.url}`);
+            const spy = router.navigate as jasmine.Spy;
+            expect(dataService.file.flowchart).toBeDefined(`Unable to load ${testName}.mob`);
+            if (dataService.file.flowchart) {
+                let nodeProcedures = 0;
+                for (const node of dataService.flowchart.nodes) {
+                    nodeProcedures += node.procedure.length;
+                }
+                expect(nodeProcedures > dataService.flowchart.nodes.length + 1).toBe(true,
+                        `${testName}.mob is an empty flowchart`);
+                if (nodeCheck) {
+                    expect(spy.calls.first().args[0][0]).toBe('/editor', 'Navigate to editor');
+                }
+                await executeFixture.componentInstance.execute(true);
+                expect(testName.split('.mob')[0]).toBe(dataService.file.name,
+                    'Loaded file name and the file name in dataService do not match.');
+                const output = dataService.flowchart.nodes[dataService.flowchart.nodes.length - 1];
+                expect(output.model).toBeDefined(
+                    `Execute fails. The end node model is not defined.`);
+                const geom_data = output.model.getGeomData();
+                if (test.requirements.hasOwnProperty('num_positions')) {
+                    expect(geom_data.num_positions).toBe(test.requirements['num_positions'], 'No. positions do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_triangles')) {
+                    expect(geom_data.triangles.length).toBe(test.requirements['num_triangles'], 'No. triangles do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_vertices')) {
+                    expect(geom_data.vertices.length).toBe(test.requirements['num_vertices'], 'No. vertices do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_edges')) {
+                    expect(geom_data.edges.length).toBe(test.requirements['num_edges'], 'No. edges do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_wires')) {
+                    expect(geom_data.wires.length).toBe(test.requirements['num_wires'], 'No. wires do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_faces')) {
+                    expect(geom_data.faces.length).toBe(test.requirements['num_faces'], 'No. faces do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_points')) {
+                    expect(geom_data.points.length).toBe(test.requirements['num_points'], 'No. points do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_polylines')) {
+                    expect(geom_data.polylines.length).toBe(test.requirements['num_polylines'], 'No. polylines do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_polygons')) {
+                    expect(geom_data.polygons.length).toBe(test.requirements['num_polygons'], 'No. polygons do not match');
+                }
+                if (test.requirements.hasOwnProperty('num_collections')) {
+                    expect(geom_data.collections.length).toBe(test.requirements['num_collections'], 'No. collections do not match');
+                }
+                if (test.returns) {
+                    expect(output.output.value).toBe(test.returns, 'Return values do not match');
+                }
+            }
+            done();
+        });
+
+    }
 });
 

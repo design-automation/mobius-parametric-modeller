4000b46460f55552c5278c47c8feddfcaec86e16
danielcai1987 <danielcai1987@gmail.com>
2019-01-28 13:55:38 +0800

    fix #277

--- src/app/model-viewers/all-viewers/gi-viewer/threejs/threejs-viewer.component.ts
+++ src/app/model-viewers/all-viewers/gi-viewer/threejs/threejs-viewer.component.ts
@@ -127,9 +127,6 @@ export class ThreejsViewerComponent implements OnInit, DoCheck, OnChanges {
                 label.updatePosition();
             });
         }
-        // self._data_threejs._renderer.clear();
-        self._data_threejs._renderer.render(self._data_threejs.basic_scene, self._data_threejs._camera);
-        self._data_threejs._renderer.clearDepth();
         self._data_threejs._renderer.render(self._data_threejs._scene, self._data_threejs._camera);
     }
 

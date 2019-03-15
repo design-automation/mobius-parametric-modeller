f15dd997a1a4abc093fbc4d27d3a9a3f71c73001
Patrick Janssen <akiphtj@nus.edu.sg>
2019-03-15 19:21:07 +0800

    fixed bug with ent exists

--- src/assets/libs/geo-info/GIGeomQuery.ts
+++ src/assets/libs/geo-info/GIGeomQuery.ts
@@ -114,6 +114,12 @@ export class GIGeomQuery {
      * @param index
      */
     public entExists(ent_type: EEntType, index: number): boolean {
+        if (ent_type === EEntType.POSI) {
+            return (
+                this._geom_arrays.up_posis_verts[index] !== undefined &&
+                this._geom_arrays.up_posis_verts[index] !== null
+            );
+        }
         const geom_arrays_key: string = EEntStrToGeomArray[ent_type];
         return (
             this._geom_arrays[geom_arrays_key][index] !== undefined &&

d9475cb42f81cc24dd2f5873b37505f3db7ec76e
danielcai1987 <danielcai1987@gmail.com>
2019-05-02 22:43:57 +0800

    Update download.ts

--- src/assets/libs/filesys/download.ts
+++ src/assets/libs/filesys/download.ts
@@ -5,29 +5,49 @@
  */
 export function download(data: string, filename: string): boolean {
     // console.log('Downloading');
-    // @ts-ignore
-    window.download = { data: data, filename: filename};
-    // @ts-ignore
-    navigator.webkitPersistentStorage.requestQuota(1024 * 1024 * 1024, function() {
-        // @ts-ignore
-        window.webkitRequestFileSystem(window.TEMPORARY , 1024 * 1024 * 1024, SaveDatFileBro);
-    });
+
+    const file = new File([data], filename, { type: 'plain/text;charset=utf-8' });
+    console.log(file.name);
+
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(file);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+
+    // // @ts-ignore
+    // window.download = { data: data, filename: filename};
+    // // @ts-ignore
+    // navigator.webkitPersistentStorage.requestQuota(1024 * 1024 * 1024, function() {
+    //     // @ts-ignore
+    //     window.webkitRequestFileSystem(window.TEMPORARY , 1024 * 1024 * 1024, SaveDatFileBro);
+    // });
+
     return true;
 }
 
 function SaveDatFileBro(localstorage) {
     // @ts-ignore
     const filename = window.download.filename;
-    localstorage.root.getFile(filename, {create: true}, function(DatFile) {
-        DatFile.createWriter(function(DatContent) {
-            const data_bom = decodeURIComponent('%ef%bb%bf');
+    localstorage.root.getFile(filename, {create: true}, function(fileEntry) {
+        fileEntry.toURL('text/plain;charset=utf-8');
+        // @ts-ignore
+        // console.log(window.download.data);
+        fileEntry.createWriter(function(fileWriter) {
+            // const data_bom = decodeURIComponent('%ef%bb%bf');
             // @ts-ignore
-            const blob = new Blob([data_bom + window.download.data], {type: 'text/plain;charset=utf-8'});
-            DatContent.write(blob);
+            const blob = new Blob([window.download.data], {type: 'text/plain;charset=utf-8'});
+            fileWriter.write(blob);
             const link = document.createElement('a');
-            link.href = 'filesystem:' + window.location.protocol + '//' + window.location.hostname + '/temporary/' + filename;
+            link.href = fileEntry.toURL('text/plain;charset=utf-8');
             link.download = filename;
             link.click();
+            // setTimeout(() => {
+            //     fileEntry.remove(() => {
+            //         console.log('File removed.');
+            //     });
+            // }, 10000);
         });
     });
     // @ts-ignore

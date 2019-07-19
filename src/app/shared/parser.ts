2ba8d3fdec31f6b56fc711851b272ec69add86f3
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-07-19 10:18:27 +0800

    infinity as special variable

--- src/app/shared/parser.ts
+++ src/app/shared/parser.ts
@@ -25,7 +25,7 @@ const otherSymbols = new Set(['.', '#', ',']);
 const noSpaceBefore = new Set(['@', ',', ']', '[']);
 
 const allConstants = (<string[][]>inline_func[0][1]).map(constComp => constComp[0]);
-const specialVars = new Set(['undefined', 'null'].concat(allConstants));
+const specialVars = new Set(['undefined', 'null', 'infinity'].concat(allConstants));
 
 const reservedWords = [
     'abstract', 'arguments', 'await', 'boolean',

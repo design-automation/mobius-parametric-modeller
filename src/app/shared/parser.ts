9b3ddc6358047375a6d47324fc6523d58412fb12
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-08-05 11:22:05 +0800

    remove excess console log

--- src/app/shared/parser.ts
+++ src/app/shared/parser.ts
@@ -292,7 +292,6 @@ export function parseArgument(str: string): {'error'?: string, 'vars'?: string[]
     let newString = '';
     let jsString = '';
     const check = analyzeComp(comps, 0, vars);
-    console.log(check.jsStr);
     if (check.error) {
         console.log(check.error, '\n', str);
         return check;

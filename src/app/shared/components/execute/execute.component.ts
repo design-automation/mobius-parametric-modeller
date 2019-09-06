5aae952a6befea60a06191574d9130d7dd5266f6
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-09-06 13:14:00 +0800

    update

--- src/app/shared/components/execute/execute.component.ts
+++ src/app/shared/components/execute/execute.component.ts
@@ -104,10 +104,13 @@ export class ExecuteComponent {
                         if (backup_list.indexOf(val) !== -1) {
                             const result = await SaveFileComponent.loadFromFileSystem(val);
                             if (!result || result === 'error') {
-                                prod.resolvedValue = arg.value;
+                                throw(new Error(`File named ${val} does not exist in the local storage`));
+                                // prod.resolvedValue = arg.value;
                             } else {
                                 prod.resolvedValue = '`' + result + '`';
                             }
+                        } else {
+                            throw(new Error(`File named ${val} does not exist in the local storage`));
                         }
                     }
                     break;
@@ -144,7 +147,7 @@ export class ExecuteComponent {
                 this.dataService.flagModifiedNode(this.dataService.flowchart.nodes[0].id);
                 document.getElementById('spinner-off').click();
                 document.getElementById('Console').click();
-                this.dataService.log(ex.message);
+                this.dataService.log(`<h4 style="padding: 2px 0px 2px 0px; color:red;">Error: ${ex.message}</h4>`);
                 const _category = this.isDev ? 'dev' : 'execute';
                 this.googleAnalyticsService.trackEvent(_category, `error: ${ex.name}`, 'click', performance.now() - this.startTime);
                 throw ex;
@@ -174,7 +177,7 @@ export class ExecuteComponent {
                             const _category = this.isDev ? 'dev' : 'execute';
                             this.googleAnalyticsService.trackEvent(_category, `error: Reserved Word Argument`,
                                 'click', performance.now() - this.startTime);
-                            this.dataService.log(ex.message);
+                            this.dataService.log(`<h4 style="padding: 2px 0px 2px 0px; color:red;">Error: ${ex.message}</h4>`);
                             throw(ex);
                         }
                         InvalidECheck = true;

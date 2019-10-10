047f80c587b8f3b5be94b5cae20435c73e6a615b
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2019-10-10 13:28:53 +0800

    script update

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "23539c19",
-    "hash": "23539c19",
+    "dirty": true,
+    "raw": "dd56dc0e-dirty",
+    "hash": "dd56dc0e",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "23539c19",
+    "suffix": "dd56dc0e-dirty",
     "semverString": null,
     "version": "0.5.6"
 };

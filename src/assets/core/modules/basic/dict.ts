7a321c94113b126abb5e303c50a62a248d90f7a2
Patrick Janssen <akiphtj@nus.edu.sg>
2019-11-06 12:25:28 +0800

    updated dictAdd

--- src/assets/core/modules/basic/dict.ts
+++ src/assets/core/modules/basic/dict.ts
@@ -17,26 +17,25 @@ import { checkCommTypes, TypeCheckObj } from '../_check_args';
  * Adds one or more key-value pairs to a dict. Existing keys with the same name will be overwritten.
  * ~
  * @param dict Dictionary to add the key-value to.
- * @param key The new key
- * @param value The new value
- * @param method Enum, select the method.
+ * @param keys_values A key-value pair, or list of key-value pairs.
  * @returns void
  */
-export function Add(dict: any[], key: string|string[], value: any): void {
+export function Add(dict: any[], keys_values: [string, any]|[string, any][]): void {
     // --- Error Check ---
     const fn_name = 'dict.Add';
-    checkCommTypes(fn_name, 'key', key, [TypeCheckObj.isString, TypeCheckObj.isStringList]);
-    checkCommTypes(fn_name, 'value', value, [TypeCheckObj.isAny]);
+    checkCommTypes(fn_name, 'key_value', keys_values, [TypeCheckObj.isList]);
     // --- Error Check ---
-    if (Array.isArray(key)) {
-        if (!Array.isArray(value) && key.length !== value.length) {
-            throw new Error('dict.Add: If a list of keys of keys is provided, then the values must also be a list of teh same length.');
+    if (keys_values.length > 0) {
+        if (!Array.isArray(keys_values[0])) { keys_values = [keys_values] as [string, any][]; }
+    }
+    for (const pair of keys_values) {
+        if (pair.length !== 2) {
+            throw new Error('dict.Add: Key-value pairs must be of length 2; the following key-value pair is not valid: "' + pair + '".');
         }
-        for (let i = 0; i < key.length; i++) {
-            dict[key[i]] = value[i];
+        if (typeof pair[0] !== 'string') {
+            throw new Error('dict.Add: Keys must be of type string; the following key is not valid: "' + pair[0] + '".');
         }
-    } else {
-        dict[key] = value;
+        dict[pair[0]] = pair[1];
     }
 }
 // ================================================================================================

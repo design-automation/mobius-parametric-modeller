62712f51a0ba122f13f69467a1f6ab2077e614ac
Patrick Janssen <akiphtj@nus.edu.sg>
2020-01-09 00:59:43 +0800

    fixed speed issue in model compare

--- src/assets/core/modules/basic/util.ts
+++ src/assets/core/modules/basic/util.ts
@@ -147,6 +147,12 @@ export enum _ECOmpareMethod {
 /**
  * Compare the GI data in this model to the GI data in another model.
  * ~
+ * If method = subset, then this model is the answer, and the other model is the submitted model.
+ * It will check that all entites in this model also exist in the other model.
+ * ~
+ * If method = superset, then this model is the submitted model, and the other model is the answer model.
+ * It will check that all entites in the other model also exist in this model.
+ * ~
  * For specifying the location of the GI Model, you can either specify a URL,
  * or the name of a file in LocalStorage.
  * In the latter case, you do not specify a path, you just specify the file name, e.g. 'my_model.gi'
@@ -160,6 +166,10 @@ export function ModelCompare(__model__: GIModel, gi_model: string, method: _ECOm
     const gi_obj: IModelData = JSON.parse(gi_model) as IModelData;
     const other_model = new GIModel(gi_obj);
     let result: {score: number, total: number, comment: string} = null;
+    // compare function has three boolean args
+    // normalize: boolean
+    // check_geom_equality: boolean
+    // check_attrib_equality: boolean
     switch (method) {
         case _ECOmpareMethod.THIS_IS_SUBSET:
             result = __model__.compare(other_model, true, false, false);

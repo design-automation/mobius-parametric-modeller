7eeaeec6b0a0fcb47940055a3a13df1a5176599d
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2020-03-19 16:35:32 +0800

    layer + terrain update

--- src/app/model-viewers/all-viewers/gi-cesium-viewer/data/data.cesium.ts
+++ src/app/model-viewers/all-viewers/gi-cesium-viewer/data/data.cesium.ts
@@ -19,11 +19,8 @@ export class DataCesium {
     public _text: string;
     public _primitives: any[];
     public _camera: any[];
-    public _viewModel = {
-        layers : [],
-        baseLayers : [],
-        selectedLayer : null
-    };
+    public _viewLayerProviders = [];
+    public _viewTerrainProviders = [];
     // interaction and selection
     // text labels
     // number of cesium points, lines, triangles
@@ -34,6 +31,8 @@ export class DataCesium {
      */
     constructor(settings: CesiumSettings) {
         this.settings = JSON.parse(JSON.stringify(settings));
+        this._getLayers();
+        this._getTerrains();
         // renderer
         // camera settings
         // orbit controls
@@ -48,27 +47,27 @@ export class DataCesium {
      */
     public createCesiumViewer() {
         // add Cesium Access Token
-        // if (this.settings.cesium.ion !== Cesium.Ion.defaultAccessToken && this.settings.cesium.ion !== '') {
-        //     Cesium.Ion.defaultAccessToken = this.settings.cesium.ion;
-        // }
+        if (this.settings.cesium.ion !== Cesium.Ion.defaultAccessToken && this.settings.cesium.ion !== '') {
+            Cesium.Ion.defaultAccessToken = this.settings.cesium.ion;
+        }
 
-        Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
-                                        '.eyJqdGkiOiI2MGMxNGYwMS1jZjYyLTQyNjM' +
-                                        'tOGNkYy1hOTRiYTk4ZGEzZDUiLCJpZCI6MTY' +
-                                        '4MSwiaWF0IjoxNTI5NTY4OTc4fQ.lL2fzwOZ' +
-                                        '6EQuL5BqXG5qIwlBn-P_DTbClhVYCIyCgS0';
+        // Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
+        //                                 '.eyJqdGkiOiI2MGMxNGYwMS1jZjYyLTQyNjM' +
+        //                                 'tOGNkYy1hOTRiYTk4ZGEzZDUiLCJpZCI6MTY' +
+        //                                 '4MSwiaWF0IjoxNTI5NTY4OTc4fQ.lL2fzwOZ' +
+        //                                 '6EQuL5BqXG5qIwlBn-P_DTbClhVYCIyCgS0';
         // create the viewer
         // https://cesiumjs.org/Cesium/Build/Documentation/Viewer.html
         // https://cesium.com/docs/tutorials/getting-started/
         // https://cesium.com/blog/2018/03/12/cesium-and-angular/
-        const view_models = this._getImageryViewModels();
+        // const view_models = this._getImageryViewModels();
         this._viewer = new Cesium.Viewer(
             document.getElementById('cesium-container'),
             {
                 shadows : true,
                 terrainShadows: Cesium.ShadowMode.ENABLED,
                 scene3DOnly: false,
-                baseLayerPicker: true,
+                baseLayerPicker: false,
                 sceneModePicker: false,
                 homeButton: true,
                 navigationHelpButton: false,
@@ -76,8 +75,8 @@ export class DataCesium {
                 animation: false,
                 timeline: true,
                 geocoder: false,
-                imageryProviderViewModels : view_models,
-                selectedImageryProviderViewModel : view_models[0],
+                // imageryProviderViewModels : view_models,
+                // selectedImageryProviderViewModel : view_models[0],
                 // terrainProviderViewModels : terrainViewModels
                 // selectedTerrainProviderViewModel : terrainViewModels[1]
             }
@@ -88,6 +87,26 @@ export class DataCesium {
         this._viewer.shadowMap.size = 2048;
         this._viewer.shadowMap.softShadows = false; // if true, causes some strange effects
         // document.getElementsByClassName('cesium-viewer-bottom')[0].remove();
+
+        if (this.settings.imagery) {
+            if (this.settings.imagery.layer) {
+                for (const layerProvider of this._viewLayerProviders) {
+                    if (layerProvider.name === this.settings.imagery.layer) {
+                        const viewer_layers = this._viewer.imageryLayers;
+                        const newLayer = new Cesium.ImageryLayer(layerProvider.creationCommand());
+                        viewer_layers.removeAll();
+                        viewer_layers.add(newLayer);
+                    }
+                }
+            }
+            if (this.settings.imagery.terrain) {
+                for (const terrainProvider of this._viewLayerProviders) {
+                    if (terrainProvider.name === this.settings.imagery.terrain) {
+                        this._viewer.terrainProvider = terrainProvider.creationCommand();
+                    }
+                }
+            }
+        }
 
         // prevent camera goes underground #375
         const handler = this._viewer.screenSpaceEventHandler;
@@ -130,17 +149,19 @@ export class DataCesium {
         // tslint:disable-next-line
         homeBtn.getElementsByTagName('path')[0].setAttribute('d', 'M15 3l2.3 2.3-2.89 2.87 1.42 1.42L18.7 6.7 21 9V3zM3 9l2.3-2.3 2.87 2.89 1.42-1.42L6.7 5.3 9 3H3zm6 12l-2.3-2.3 2.89-2.87-1.42-1.42L5.3 17.3 3 15v6zm12-6l-2.3 2.3-2.87-2.89-1.42 1.42 2.89 2.87L15 21h6z');
         homeBtn.title = 'Zoom to Fit Model';
+        homeBtn.style.top = '30px';
+        homeBtn.style.right = '0px';
         // settings button
-        const settingsBtn = homeBtn.nextElementSibling as HTMLElement;
-        settingsBtn.getElementsByTagName('img')[0].remove();
-        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        // const settingsBtn = homeBtn.nextElementSibling as HTMLElement;
+        // settingsBtn.getElementsByTagName('img')[0].remove();
+        // const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        // const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         // tslint:disable-next-line
         // path.setAttribute('d', 'M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z')
         // tslint:disable-next-line
-        path.setAttribute('d', 'M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z')
-        svg.append(path);
-        settingsBtn.append(svg);
+        // path.setAttribute('d', 'M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z')
+        // svg.append(path);
+        // settingsBtn.append(svg);
         // settingsBtn.style.top = '58px';
         // const btn = document.getElementById('attribToggle');
         // if (btn) {
@@ -497,6 +518,49 @@ export class DataCesium {
         }
     }
 
+    public updateSettings(settings) {
+        const newSetting = <CesiumSettings> JSON.parse(JSON.stringify(settings));
+        newSetting.cesium.ion = newSetting.cesium.ion.trim();
+        if (newSetting.cesium) {
+            if (newSetting.cesium.ion !== Cesium.Ion.defaultAccessToken && newSetting.cesium.ion !== '') {
+                Cesium.Ion.defaultAccessToken = newSetting.cesium.ion;
+            }
+            if (newSetting.cesium.hasOwnProperty('save')) {
+                this.settings.cesium.save = newSetting.cesium.save;
+                if (this.settings.cesium.save) {
+                    this.settings.cesium.ion = newSetting.cesium.ion;
+                    this.settings.cesium.assetid = newSetting.cesium.assetid;
+                }
+            }
+        }
+        if (newSetting.imagery) {
+            if (newSetting.imagery.layer) {
+                for (const layerProvider of this._viewLayerProviders) {
+                    if (layerProvider.name === newSetting.imagery.layer) {
+                        const viewer_layers = this._viewer.imageryLayers;
+                        const newLayer = new Cesium.ImageryLayer(layerProvider.creationCommand());
+                        viewer_layers.removeAll();
+                        viewer_layers.add(newLayer);
+                        this.settings.imagery.layer = newSetting.imagery.layer;
+                    }
+                }
+            }
+            if (newSetting.imagery.terrain) {
+                for (const terrainProvider of this._viewTerrainProviders) {
+                    if (terrainProvider.name === newSetting.imagery.terrain) {
+                        this._viewer.terrainProvider = terrainProvider.creationCommand();
+                        this.settings.imagery.terrain = newSetting.imagery.terrain;
+                    }
+                }
+            }
+        }
+        if (Cesium.Ion.defaultAccessToken && newSetting.cesium && newSetting.cesium.assetid) {
+            this._addAssets(newSetting.cesium.assetid);
+        }
+        localStorage.setItem('cesium_settings', JSON.stringify(this.settings));
+    }
+
+
     // PRIVATE METHODS
     /**
      * Get a set of image layers
@@ -595,6 +659,80 @@ export class DataCesium {
         // }));
         return view_models;
     }
+
+    private _getLayers() {
+        this._viewLayerProviders = [];
+        this._viewLayerProviders.push(new Cesium.ProviderViewModel({
+            name: 'Open Street Map',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/openStreetMap.png'),
+            tooltip: 'OpenStreetMap (OSM) is a collaborative project to create a free editable \
+                 map of the world.\nhttp://www.openstreetmap.org',
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: 'https://a.tile.openstreetmap.org/',
+                });
+            },
+        }));
+        this._viewLayerProviders.push(new Cesium.ProviderViewModel({
+            name: 'Stamen Toner',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/stamenToner.png'),
+            tooltip: 'Stamen high contrast black and white map.\nhttp://www.maps.stamen.com/',
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: 'https://stamen-tiles.a.ssl.fastly.net/toner/',
+                });
+            },
+        }));
+        this._viewLayerProviders.push(new Cesium.ProviderViewModel({
+            name: 'Stamen Toner(Lite)',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/stamenToner.png'),
+            tooltip: 'Stamen high contrast black and white map(Lite).\nhttp://www.maps.stamen.com/',
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/',
+                });
+            },
+        }));
+        this._viewLayerProviders.push(new Cesium.ProviderViewModel({
+            name: 'Terrain(Standard)',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/TerrainProviders/CesiumWorldTerrain.png'),
+            tooltip: 'Stamen World terrain.\nhttp://www.maps.stamen.com/',
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: 'https://stamen-tiles.a.ssl.fastly.net/terrain/',
+                });
+            },
+        }));
+        this._viewLayerProviders.push(new Cesium.ProviderViewModel({
+            name: 'Terrain(Background)',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/TerrainProviders/CesiumWorldTerrain.png'),
+            tooltip: 'Stamen World terrain (Background).\nhttp://www.maps.stamen.com/',
+            creationFunction: function () {
+                return new Cesium.OpenStreetMapImageryProvider({
+                    url: 'https://stamen-tiles.a.ssl.fastly.net/terrain-background/',
+                });
+            },
+        }));
+    }
+
+    private _getTerrains() {
+        this._viewTerrainProviders.push(new Cesium.ProviderViewModel({
+            name: 'Ellipsoid Terrain',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/openStreetMap.png'),
+            tooltip: 'Ellipsoid Terrain',
+            creationFunction: function () {
+                return new Cesium.EllipsoidTerrainProvider({});
+            },
+        }));
+        this._viewTerrainProviders.push(new Cesium.ProviderViewModel({
+            name: 'Cesium World Terrain',
+            iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/stamenToner.png'),
+            tooltip: 'Cesium World Terrain',
+            creationFunction: Cesium.createWorldTerrain,
+        }));
+    }
+
+
     private _createHud(text: string) {
         const div = document.createElement('div');
         div.id = `hud`;
@@ -612,25 +750,5 @@ export class DataCesium {
         };
     }
 
-    public updateSettings(settings) {
-        const newSetting = <CesiumSettings> JSON.parse(JSON.stringify(settings));
-        newSetting.cesium.ion = newSetting.cesium.ion.trim();
-        if (newSetting.cesium) {
-            if (newSetting.cesium.ion !== Cesium.Ion.defaultAccessToken && newSetting.cesium.ion !== '') {
-                Cesium.Ion.defaultAccessToken = newSetting.cesium.ion;
-            }
-            if (newSetting.cesium.hasOwnProperty('save')) {
-                this.settings.cesium.save = newSetting.cesium.save;
-                if (this.settings.cesium.save) {
-                    this.settings.cesium.ion = newSetting.cesium.ion;
-                    this.settings.cesium.assetid = newSetting.cesium.assetid;
-                }
-            }
-        }
-        if (Cesium.Ion.defaultAccessToken && newSetting.cesium && newSetting.cesium.assetid) {
-            this._addAssets(newSetting.cesium.assetid);
-        }
-        localStorage.setItem('cesium_settings', JSON.stringify(this.settings));
-    }
 }
 

adcc7d63992a5e993be4d525199c42214ac3562e
Patrick Janssen <akiphtj@nus.edu.sg>
2020-03-19 09:17:51 +0800

    fixed doc comens for 2d clean

--- src/assets/core/modules/basic/poly2d.ts
+++ src/assets/core/modules/basic/poly2d.ts
@@ -895,10 +895,13 @@ function _offsetPline(__model__: GIModel, pline_i: number, dist: number,
 // ================================================================================================
 /**
  * Clean a polyline or polygon.
- *
+ * ~
+ * Vertices that are closer together than the specified tolerance will be merged.
+ * Vertices that are colinear within the tolerance distance will be deleted.
+ * ~
  * @param __model__
- * @param entities A list of pollines or polygons, or entities from which polylines or polygons can be extracted.
- * @param tolerance ???
+ * @param entities A list of polylines or polygons, or entities from which polylines or polygons can be extracted.
+ * @param tolerance The tolerance for deleting vertices from the polyline.
  * @returns A list of new polygons.
  */
 export function Clean(__model__: GIModel, entities: TId|TId[], tolerance: number): TId[] {

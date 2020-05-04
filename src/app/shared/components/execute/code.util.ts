b6902722b86426bd67c3d84f4a8ce3761cb44208
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2020-05-04 16:20:36 +0800

    update

--- src/app/shared/components/execute/code.util.ts
+++ src/app/shared/components/execute/code.util.ts
@@ -387,15 +387,23 @@ export class CodeUtils {
         let codeStr = [];
         let elifcount = 0;
         for (const p of prodList) {
-            codeStr = codeStr.concat(CodeUtils.getProcedureCode(p, existingVars, isMainFlowchart,
-                                                                functionName, nodeId, usedFunctions));
+            const procedureCode = CodeUtils.getProcedureCode(p, existingVars, isMainFlowchart,
+                functionName, nodeId, usedFunctions);
             if ( p.type === ProcedureTypes.Elseif && p.enabled) {
+                codeStr = codeStr.concat(procedureCode);
                 elifcount++;
+            } else if (p.type === ProcedureTypes.Else && p.enabled) {
+                codeStr = codeStr.concat(procedureCode);
+                while (elifcount > 0) {
+                    codeStr.push('}');
+                    elifcount--;
+                }
             } else {
                 while (elifcount > 0) {
                     codeStr.push('}');
                     elifcount--;
                 }
+                codeStr = codeStr.concat(procedureCode);
             }
         }
         while (elifcount > 0) {

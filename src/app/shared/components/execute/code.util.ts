a7390d44afdb9bf1ef928625fc1a0be202a1fac5
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2020-10-19 09:44:45 +0800

    nextsnapshot update

--- src/app/shared/components/execute/code.util.ts
+++ src/app/shared/components/execute/code.util.ts
@@ -750,10 +750,12 @@ export class CodeUtils {
                     }
                     activeNodes.push(`ssid_${func.name}_${nodeEdge.source.parentNode.id}`);
                 }
-                fnCode += `\nlet ssid_${nodeFuncName} = __params__.model.nextSnapshot([${activeNodes.join(',')}]);\n`;
-                // if (activeNodes.length > 1) {
-                //     fnCode += `\nlet ssid_${nodeFuncName} = __params__.model.nextSnapshot([${activeNodes.join(',')}]);\n`;
-                // }
+                // fnCode += `\nlet ssid_${nodeFuncName} = __params__.model.nextSnapshot([${activeNodes.join(',')}]);\n`;
+                if (activeNodes.length !== 1) {
+                    fnCode += `\nlet ssid_${nodeFuncName} = __params__.model.nextSnapshot([${activeNodes.join(',')}]);\n`;
+                } else {
+                    fnCode += `\nlet ssid_${nodeFuncName} = ${activeNodes[0]};\n`;
+                }
                 if (node.type === 'end') {
                     fnCode += `\nreturn await ${nodeFuncName}(__params__${func.args.map(arg => ', ' + arg.name + '_').join('')});\n`;
                 } else {

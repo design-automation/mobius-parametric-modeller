24da8c89ca70d69137da89e69bb4e7ab59188293
Bui Do Phuong Tung <akibdpt@nus.edu.sg>
2020-11-18 17:26:54 +0800

    update

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "797ff1d",
-    "hash": "797ff1d",
+    "dirty": true,
+    "raw": "af74f274-dirty",
+    "hash": "af74f274",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "797ff1d",
+    "suffix": "af74f274-dirty",
     "semverString": null,
     "version": "0.6.2"
 };

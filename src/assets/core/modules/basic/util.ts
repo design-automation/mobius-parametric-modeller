5bd19a0d863187ccbac62e29efafee8ca95d2d3b
phtj <patrick@janssen.name>
2020-12-01 15:32:50 +0800

    removed Purge and ExportIO

--- src/assets/core/modules/basic/util.ts
+++ src/assets/core/modules/basic/util.ts
@@ -5,30 +5,25 @@
 /**
  *
  */
-
 import { checkIDs, ID } from '../_check_ids';
-
 import { GIModel } from '@libs/geo-info/GIModel';
-import { download } from '@libs/filesys/download';
 import { EEntType, IModelJSONData, TId, TEntTypeIdx, EAttribNames, EAttribDataTypeStrs } from '@libs/geo-info/common';
-// import { __merge__ } from '../_model';
-// import { _model } from '..';
 import { arrMakeFlat } from '@assets/libs/util/arrs';
 import { idsBreak } from '@assets/libs/geo-info/common_id_funcs';
 import { _getFile } from './io';
 
 // ================================================================================================
 /**
- * Select entities in the GI viewer
+ * Select entities in the model.
  *
  * @param __model__
- * @param ents_id
+ * @param entities
  * @returns void
  */
-export function SelectGeometry(__model__: GIModel, ents_id: string|string[]|string[][]): void {
+export function SelectEntities(__model__: GIModel, entities: string|string[]|string[][]): void {
     __model__.modeldata.geom.selected = [];
-    ents_id = ((Array.isArray(ents_id)) ? ents_id : [ents_id]) as string[];
-    const [ents_id_flat, ents_indices] = _flatten(ents_id);
+    entities = ((Array.isArray(entities)) ? entities : [entities]) as string[];
+    const [ents_id_flat, ents_indices] = _flatten(entities);
     const ents_arr: TEntTypeIdx[] = idsBreak(ents_id_flat) as TEntTypeIdx[];
     const attrib_name = '_selected';
     for (let i = 0; i < ents_arr.length; i++) {
@@ -68,96 +63,6 @@ export enum _ECOmpareMethod {
     THIS_IS_SUBSET = 'subset',
     THIS_IS_SUPERSET = 'superset',
     THIS_IS_EQUAL = 'equal'
-}
-
-// ================================================================================================
-/**
- * Removes all deleted entities from the model.
- * The IDs of other entities may change as a result.
- * ~
- * For example, if 'pg0' was deleted and 'pg1' still exists, then after purge
- * 'pg1' will get renumbered, and will get the ID 'pg0'.
- *
- * @param __model__
- * @returns void
- */
-export function ModelPurge(__model__: GIModel): void {
-    // __model__.purge();
-    throw new Error('Not implemented');
-}
-// ================================================================================================
-/**
- * Returns an html string representation of the contents of this model
- *
- * @param __model__
- * @returns Text that summarises what is in the model, click print to see this text.
- */
-export function ModelInfo(__model__: GIModel): string {
-    let info = '<h4>Model Information:</h4>';
-    info += '<ul>';
-    // model attribs
-    const model_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.MOD);
-    if (model_attribs.length !== 0) { info += '<li>Model attribs: ' + model_attribs.join(', ') + '</li>'; }
-    // collections
-    const num_colls: number = __model__.modeldata.geom.query.numEnts(EEntType.COLL);
-    const coll_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.COLL);
-    info += '<li>';
-    info += '<b>Collections</b>: ' + num_colls; // + ' (Deleted: ' + num_del_colls + ') ';
-    if (coll_attribs.length !== 0) { info += 'Attribs: ' + coll_attribs.join(', '); }
-    info += '</li>';
-    // pgons
-    const num_pgons: number = __model__.modeldata.geom.query.numEnts(EEntType.PGON);
-    const pgon_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.PGON);
-    info += '<li>';
-    info += '<b>Polygons</b>: ' + num_pgons; // + ' (Deleted: ' + num_del_pgons + ') ';
-    if (pgon_attribs.length !== 0) { info += 'Attribs: ' + pgon_attribs.join(', '); }
-    info += '</li>';
-    // plines
-    const num_plines: number = __model__.modeldata.geom.query.numEnts(EEntType.PLINE);
-    const pline_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.PLINE);
-    info += '<li>';
-    info += '<b>Polylines</b>: ' + num_plines; // + ' (Deleted: ' + num_del_plines + ') ';
-    if (pline_attribs.length !== 0) { info += 'Attribs: ' + pline_attribs.join(', '); }
-    info += '</li>';
-    // points
-    const num_points: number = __model__.modeldata.geom.query.numEnts(EEntType.POINT);
-    const point_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.POINT);
-    info += '<li>';
-    info += '<b>Points</b>: ' + num_points; // + ' (Deleted: ' + num_del_points + ') ';
-    if (point_attribs.length !== 0) { info += 'Attribs: ' + point_attribs.join(', '); }
-    info += '</li>';
-    // wires
-    const num_wires: number = __model__.modeldata.geom.query.numEnts(EEntType.WIRE);
-    const wire_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.WIRE);
-    info += '<li>';
-    info += '<b>Wires</b>: ' + num_wires; // + ' (Deleted: ' + num_del_wires + ') ';
-    if (wire_attribs.length !== 0) { info += 'Attribs: ' + wire_attribs.join(', '); }
-    info += '</li>';
-    // edges
-    const num_edges: number = __model__.modeldata.geom.query.numEnts(EEntType.EDGE);
-    const edge_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.EDGE);
-    info += '<li>';
-    info += '<b>Edges</b>: ' + num_edges; // + ' (Deleted: ' + num_del_edges + ') ';
-    if (edge_attribs.length !== 0) { info += 'Attribs: ' + edge_attribs.join(', '); }
-    info += '</li>';
-    // verts
-    const num_verts: number = __model__.modeldata.geom.query.numEnts(EEntType.VERT);
-    const vert_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.VERT);
-    info += '<li>';
-    info += '<b>Vertices</b>: ' + num_verts; // + ' (Deleted: ' + num_del_verts + ') ';
-    if (vert_attribs.length !== 0) { info += 'Attribs: ' + vert_attribs.join(', '); }
-    info += '</li>';
-    // posis
-    const num_posis: number = __model__.modeldata.geom.query.numEnts(EEntType.POSI);
-    const posi_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.POSI);
-    info += '<li>';
-    info += '<b>Positions</b>: ' + num_posis; // + ' (Deleted: ' + num_del_posis + ') ';
-    if (posi_attribs.length !== 0) { info += 'Attribs: ' + posi_attribs.join(', '); }
-    info += '</li>';
-    // end
-    info += '</ul>';
-    // return the string
-    return info;
 }
 // ================================================================================================
 /**
@@ -353,88 +258,101 @@ function _collInfo(__model__: GIModel, coll_i: number): string {
     return info;
 }
 // ================================================================================================
-
-export enum _EIOExportParams {
-    YES = 'Add Params',
-    NO = 'No Params'
-}
-export enum _EIOExportContents {
-    BOTH = 'Both',
-    CONSOLE = 'Console Only',
-    MODEL = 'Model Only'
-}
 /**
- * Export data from the model as a file.
- * This will result in a popup in your browser, asking you to save the filel.
+ * Returns an html string representation of the contents of this model
+ *
  * @param __model__
- * @param __console__
- * @param __constList__
- * @param __fileName__
- * @param file_name Name of the file as a string.
- * @param exportParams Enum.
- * @param exportContent Enum.
- * @returns Boolean.
- * @example util.ExportIO('my_model.json')
- * @example_info Exports all the data in the model as an OBJ.
+ * @returns Text that summarises what is in the model, click print to see this text.
  */
-export function ExportIO(__model__: GIModel, __console__: string[], __constList__: any, __fileName__: string,
-                        file_name: string, exportParams: _EIOExportParams, exportContent: _EIOExportContents): boolean {
-    // TODO
-    throw new Error("Not implemented");
-    // TODO
-
-    // // let gi_data: string = JSON.stringify(__model__.getData());
-    // // gi_data = gi_data.replace(/\\\"/g, '\\\\\\"'); // TODO temporary fix
-    // const consolidatedConsole = [];
-    // for (const logStr of __console__) {
-    //     if (!logStr.match('<p style="padding: 2px 0px 2px 10px;"><b><i>')) {
-    //         continue;
-    //     }
-    //     const replacedStr = logStr.replace('<p style="padding: 2px 0px 2px 10px;"><b><i>', '')
-    //                            .replace('</i></b> ', '').replace('</p>', '').replace('<br>', '\n');
-    //     consolidatedConsole.push(replacedStr);
-    // }
-    // const newConstList = {};
-    // for (const obj in __constList__) {
-    //     if (__constList__.hasOwnProperty(obj)) {
-    //         newConstList[obj] = convertString(__constList__[obj]);
-    //     }
-    // }
-    // const edxAnswer = {
-    //     'fileName': __fileName__,
-    //     'params' : newConstList,
-    //     'console': consolidatedConsole.join('\n'),
-    //     'model'  : __model__.getModelData(__model__.modeldata.active_snapshot)
-    // };
-    // if (exportParams === _EIOExportParams.NO) {
-    //     edxAnswer['params'] = undefined;
-    // }
-    // if (exportContent === _EIOExportContents.CONSOLE) {
-    //     edxAnswer['model'] = undefined;
-    // } else if (exportContent === _EIOExportContents.MODEL) {
-    //     edxAnswer['console'] = undefined;
-    // }
-
-    // return download(JSON.stringify(edxAnswer) , file_name);
-}
-function convertString(value) {
-    let val;
-    if (!value) {
-        val = value;
-    } else if (typeof value === 'number' || value === undefined) {
-        val = value;
-    } else if (typeof value === 'string') {
-        val = '"' + value + '"';
-    } else if (value.constructor === [].constructor) {
-        val = JSON.stringify(value);
-    } else if (value.constructor === {}.constructor) {
-        val = JSON.stringify(value);
-    } else {
-        val = value;
-    }
-    return val;
+export function ModelInfo(__model__: GIModel): string {
+    let info = '<h4>Model Information:</h4>';
+    info += '<ul>';
+    // model attribs
+    const model_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.MOD);
+    if (model_attribs.length !== 0) { info += '<li>Model attribs: ' + model_attribs.join(', ') + '</li>'; }
+    // collections
+    const num_colls: number = __model__.modeldata.geom.query.numEnts(EEntType.COLL);
+    const coll_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.COLL);
+    info += '<li>';
+    info += '<b>Collections</b>: ' + num_colls; // + ' (Deleted: ' + num_del_colls + ') ';
+    if (coll_attribs.length !== 0) { info += 'Attribs: ' + coll_attribs.join(', '); }
+    info += '</li>';
+    // pgons
+    const num_pgons: number = __model__.modeldata.geom.query.numEnts(EEntType.PGON);
+    const pgon_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.PGON);
+    info += '<li>';
+    info += '<b>Polygons</b>: ' + num_pgons; // + ' (Deleted: ' + num_del_pgons + ') ';
+    if (pgon_attribs.length !== 0) { info += 'Attribs: ' + pgon_attribs.join(', '); }
+    info += '</li>';
+    // plines
+    const num_plines: number = __model__.modeldata.geom.query.numEnts(EEntType.PLINE);
+    const pline_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.PLINE);
+    info += '<li>';
+    info += '<b>Polylines</b>: ' + num_plines; // + ' (Deleted: ' + num_del_plines + ') ';
+    if (pline_attribs.length !== 0) { info += 'Attribs: ' + pline_attribs.join(', '); }
+    info += '</li>';
+    // points
+    const num_points: number = __model__.modeldata.geom.query.numEnts(EEntType.POINT);
+    const point_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.POINT);
+    info += '<li>';
+    info += '<b>Points</b>: ' + num_points; // + ' (Deleted: ' + num_del_points + ') ';
+    if (point_attribs.length !== 0) { info += 'Attribs: ' + point_attribs.join(', '); }
+    info += '</li>';
+    // wires
+    const num_wires: number = __model__.modeldata.geom.query.numEnts(EEntType.WIRE);
+    const wire_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.WIRE);
+    info += '<li>';
+    info += '<b>Wires</b>: ' + num_wires; // + ' (Deleted: ' + num_del_wires + ') ';
+    if (wire_attribs.length !== 0) { info += 'Attribs: ' + wire_attribs.join(', '); }
+    info += '</li>';
+    // edges
+    const num_edges: number = __model__.modeldata.geom.query.numEnts(EEntType.EDGE);
+    const edge_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.EDGE);
+    info += '<li>';
+    info += '<b>Edges</b>: ' + num_edges; // + ' (Deleted: ' + num_del_edges + ') ';
+    if (edge_attribs.length !== 0) { info += 'Attribs: ' + edge_attribs.join(', '); }
+    info += '</li>';
+    // verts
+    const num_verts: number = __model__.modeldata.geom.query.numEnts(EEntType.VERT);
+    const vert_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.VERT);
+    info += '<li>';
+    info += '<b>Vertices</b>: ' + num_verts; // + ' (Deleted: ' + num_del_verts + ') ';
+    if (vert_attribs.length !== 0) { info += 'Attribs: ' + vert_attribs.join(', '); }
+    info += '</li>';
+    // posis
+    const num_posis: number = __model__.modeldata.geom.query.numEnts(EEntType.POSI);
+    const posi_attribs: string[] = __model__.modeldata.attribs.getAttribNames(EEntType.POSI);
+    info += '<li>';
+    info += '<b>Positions</b>: ' + num_posis; // + ' (Deleted: ' + num_del_posis + ') ';
+    if (posi_attribs.length !== 0) { info += 'Attribs: ' + posi_attribs.join(', '); }
+    info += '</li>';
+    // end
+    info += '</ul>';
+    // return the string
+    return info;
 }
 // ================================================================================================
+/**
+ * Check the internal consistency of the model.
+ *
+ * @param __model__
+ * @returns Text that summarises what is in the model, click print to see this text.
+ */
+export function ModelCheck(__model__: GIModel): string {
+    console.log('==== ==== ==== ====');
+    console.log('MODEL GEOM\n', __model__.modeldata.geom.toStr());
+    // console.log('MODEL ATTRIBS\n', __model__.modeldata.attribs.toStr());
+    console.log('META\n', __model__.metadata.toDebugStr());
+    console.log('==== ==== ==== ====');
+    console.log(__model__);
+    const check: string[] = __model__.check();
+    if (check.length > 0) {
+        return String(check);
+    }
+    return 'No internal inconsistencies have been found.';
+}
+// ================================================================================================
+
 /**
  * Compare the GI data in this model to the GI data in another model.
  * ~
@@ -478,26 +396,6 @@ export async function ModelCompare(__model__: GIModel, input_data: string, metho
     //         throw new Error('Compare method not recognised');
     // }
     // return result.comment;
-}
-// ================================================================================================
-/**
- * Check the internal consistency of the model.
- *
- * @param __model__
- * @returns Text that summarises what is in the model, click print to see this text.
- */
-export function ModelCheck(__model__: GIModel): string {
-    console.log('==== ==== ==== ====');
-    console.log('MODEL GEOM\n', __model__.modeldata.geom.toStr());
-    // console.log('MODEL ATTRIBS\n', __model__.modeldata.attribs.toStr());
-    console.log('META\n', __model__.metadata.toDebugStr());
-    console.log('==== ==== ==== ====');
-    console.log(__model__);
-    const check: string[] = __model__.check();
-    if (check.length > 0) {
-        return String(check);
-    }
-    return 'No internal inconsistencies have been found.';
 }
 // ================================================================================================
 /**

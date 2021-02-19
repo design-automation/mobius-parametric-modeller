1a262fdda4cb3d6f1ebe4adef7946355bbe6570e
phuongtung1 <phuongtung1231992@yahoo.com>
2021-02-19 13:31:27 +0800

    fetch update to catch error

--- src/assets/core/modules/basic/io.ts
+++ src/assets/core/modules/basic/io.ts
@@ -412,7 +412,11 @@ async function getURLContent(url: string): Promise<any> {
         url = url.substring(0, url.length - 1);
     }
     const p = new Promise((resolve) => {
-        fetch(url).then(res => {
+        const fetchObj = fetch(url);
+        fetchObj.catch(err => {
+            resolve('HTTP Request Error: Unable to retrieve file from ' + url);
+        });
+        fetchObj.then(res => {
             if (!res.ok) {
                 resolve('HTTP Request Error: Unable to retrieve file from ' + url);
                 return '';
